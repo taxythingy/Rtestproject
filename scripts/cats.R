be8a51acd78b209fc0b4ea2f121ef5b56d2df474
Chris Dunlop <chris.dunlop@outlook.com>
2016-10-18 14:23:26 +1300

    added newer data file Added scripts for cleaning up data file

--- scripts/cats.R
+++ scripts/cats.R
@@ -1,1 +1,5 @@
-cats <- read.csv("data/feline-data.csv", header = TRUE)
+cats <- read.csv("data/feline-data2.csv", header = TRUE)
+# remove row of damaged data
+cats <- cats[-4,]
+# convert coerced column from factors to numeric (double)
+cats$weight <- as.numeric(as.character(cats$weight))
