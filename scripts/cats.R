ff8ca87214221ce42f6b999ee714d238ab5a31d3
Chris Dunlop <chris.dunlop@outlook.com>
2016-10-18 16:33:35 +1300

    functions

--- scripts/cats.R
+++ scripts/cats.R
@@ -35,14 +35,36 @@ compare_to_nine <- function(val) {
     print("More!")
   } else if (val == 9) {
     print("Equal!")
-  } else if (val < 9) 
+  } else if (val < 9) {
     print("Less!")
   } else {
     print("Unknown evaluation. Something went wrong!")
   }
 }
 
+# Couple of quick helper functions
+any(cats$weight > 4)
+all(cats$weight > 1)
 
+# using active locations in vectors
+cats[cats$weight > 4,]
+
+# for loop
+for (i in 1:10) {
+  print(i)
+}
+
+# nesting functions - better practice examples
+
+compare_all_to_nine <- function(x) {
+  for (i in x) {
+    compare_to_nine(i)
+  }
+}
+
+compare_all_to_nine(c(1:10)) # assess vector of numbers using function
+
+sapply(c(1:10),compare_to_nine) # simple run of vector over function. Note no () or var.
 
 
 
